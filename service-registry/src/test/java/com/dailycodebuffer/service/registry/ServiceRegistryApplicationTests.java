4111ff9d34e44e710f2f6e71f6dbceaaf181a3b1
Renz <crc1046@dlsud.edu.ph>
2023-04-06 08:00:32 +0800

    refactor: replaced all init files

--- service-registry/src/test/java/com/dailycodebuffer/service/registry/ServiceRegistryApplicationTests.java
+++
@@ -1,9 +0,0 @@
-package com.dailycodebuffer.service.registry;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class ServiceRegistryApplicationTests {
-
-}
